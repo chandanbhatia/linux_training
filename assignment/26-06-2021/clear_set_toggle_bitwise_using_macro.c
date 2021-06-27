6c0501ec03fce3f021e356d2b775fce50df1abf4
Chandan Bhatia <chandan.bhatia17@gmail.com>
2021-06-27 11:01:37 +0530

    Create clear_set_toggle_bitwise_using_macro.c

---
+++ assignment/26-06-2021/clear_set_toggle_bitwise_using_macro.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define clearBit(val, position) (val = val & ~(0x01 << position))
+
+#define setBit(val, position) (val = val | (0x01 << position) ) 
+
+#define toggleBit(val, position) ( val = val ^ (0x01<<position))
+
+int main() {
+    unsigned int data = 0xff, position = 2;
+    printf("input data  is 0x%x \n", data);
+    printf("post clear bit %d, data is 0x%x \n", position, clearBit(data, position));
+    
+    data = 0x80;
+    position = 0;
+    printf("input data  is 0x%x \n", data);
+    printf("post set bit %d, data is 0x%x \n", position, setBit(data, position));
+    
+    data = 0xff;
+    position = 0;
+    printf("input data  is 0x%x \n", data);
+    printf("post toggle bit %d, data is 0x%x \n", position, toggleBit(data, position));
+     return 0;
+}
+
+/*************************************************************************************************
+Test Case 1:
+  input data  is 0xff 
+  post clear bit 2, data is 0xfb 
+  input data  is 0x80 
+  post set bit 0, data is 0x81 
+  input data  is 0xff 
+  post toggle bit 0, data is 0xfe
+  
+ Test Case 2:
+  input data  is 0xfffff 
+  post clear bit 10, data is 0xffbff 
+  input data  is 0xf980 
+  post set bit 10, data is 0xfd80 
+  input data  is 0x81 
+  post toggle bit 0, data is 0x80 
+*************************************************************************************************/
